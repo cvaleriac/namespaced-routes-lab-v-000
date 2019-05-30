8b3a626e190733b2fdaac7139862ec7ba8ba2796
valeria campilongo <cvaleriac@hotmail.it>
2019-05-30 16:35:48 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
 
-  namespace :admin do
-   resources :preferences, only: [:index, :update]
- end
- resources :artists do
-   resources :songs, only: [:index, :show]
- end
- resources :songs
+  resources :preferences
+  resources :artists do	  resources :artists do
+    resources :songs, only: [:index, :show]	    resources :songs, only: [:index, :show]
+  end	  end
+  resources :songs	  resources :songs
+
+
+   namespace :admin do
+    resources :preferences, only: [:index]
+  end
 
  end  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
