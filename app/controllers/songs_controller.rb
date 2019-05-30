945a60588245a15784ba4377eb28bc67b9c0e4b4
valeria campilongo <cvaleriac@hotmail.it>
2019-05-30 16:36:08 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,70 +1,73 @@
 class SongsController < ApplicationController
   def index
-    if params[:artist_id]
-      @artist = Artist.find_by(id: params[:artist_id])
-      if @artist.nil?
-        redirect_to artists_path, alert: "Artist not found"
+      if params[:artist_id]
+        @artist = Artist.find_by(id: params[:artist_id])
+        if @artist.nil?
+          redirect_to artists_path, alert: "Artist not found"
+        else
+          @songs = @artist.songs
+        end
       else
-        @songs = @artist.songs
+        @songs = Song.all
       end
-    else
-      @songs = Song.all
     end
-  end
 
-  def show
-    if params[:artist_id]
-      @artist = Artist.find_by(id: params[:artist_id])
-      @song = @artist.songs.find_by(id: params[:id])
-      if @song.nil?
-        redirect_to artist_songs_path(@artist), alert: "Song not found"
+    def show
+      if params[:artist_id]
+        @artist = Artist.find_by(id: params[:artist_id])
+        @song = @artist.songs.find_by(id: params[:id])
+        if @song.nil?
+          redirect_to artist_songs_path(@artist), alert: "Song not found"
+        end
+      else
+        @song = Song.find(params[:id])
       end
-    else
+    end
+
+    def new
+      if Preference.find_by(allow_create_songs: false)
+        redirect_to songs_path
+      else
+        @song = Song.new
+      end
+    end
+
+    def create
+      @song = Song.new(song_params)
+
+      if @song.save
+        redirect_to @song
+      else
+        render :new
+      end
+    end
+
+    def edit
       @song = Song.find(params[:id])
     end
-  end
 
-  def new
-    @song = Song.new
-  end
+    def update
+      @song = Song.find(params[:id])
 
-  def create
-    @song = Song.new(song_params)
+      @song.update(song_params)
 
-    if @song.save
-      redirect_to @song
-    else
-      render :new
+      if @song.save
+        redirect_to @song
+      else
+        render :edit
+      end
     end
-  end
 
-  def edit
-    @song = Song.find(params[:id])
-  end
-
-  def update
-    @song = Song.find(params[:id])
-
-    @song.update(song_params)
-
-    if @song.save
-      redirect_to @song
-    else
-      render :edit
+    def destroy
+      @song = Song.find(params[:id])
+      @song.destroy
+      flash[:notice] = "Song deleted."
+      redirect_to songs_path
     end
-  end
 
-  def destroy
-    @song = Song.find(params[:id])
-    @song.destroy
-    flash[:notice] = "Song deleted."
-    redirect_to songs_path
-  end
+    private
 
-  private
-
-  def song_params
-    params.require(:song).permit(:title, :artist_name)
-  end
+    def song_params
+      params.require(:song).permit(:title, :artist_name)
+    end
 end
-
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
+  resources :preferences
+  resources :artists do	  resources :artists do
+    resources :songs, only: [:index, :show]	    resources :songs, only: [:index, :show]
+  end	  end
+  resources :songs	  resources :songs
+
+
+   namespace :admin do
+    resources :preferences, only: [:index]
+  end
+
+ end  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
