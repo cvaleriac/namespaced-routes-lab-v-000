edf5b94e2423815c7d07063b0dff9128850b518e
valeria campilongo <cvaleriac@hotmail.it>
2019-05-30 16:33:41 +0000

    Automatically backed up by Learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,56 +1,56 @@
 class ArtistsController < ApplicationController
   def index
-    @artists = Artist.all.order("name " + preference.artist_sort_order)
-  end
+      @artists = Artist.all
+    end
 
-  def show
-    @artist = Artist.find(params[:id])
-  end
+    def show
+      @artist = Artist.find(params[:id])
+    end
 
-  def new
-    if preference.allow_create_artists
-      @artist = Artist.new
-    else
+    def new
+      if Preference.find_by(allow_create_artists: false)
+        redirect_to artists_path
+      else
+        @artist = Artist.new
+      end
+    end
+
+    def create
+      @artist = Artist.new(artist_params)
+
+      if @artist.save
+        redirect_to @artist
+      else
+        render :new
+      end
+    end
+
+    def edit
+      @artist = Artist.find(params[:id])
+    end
+
+    def update
+      @artist = Artist.find(params[:id])
+
+      @artist.update(artist_params)
+
+      if @artist.save
+        redirect_to @artist
+      else
+        render :edit
+      end
+    end
+
+    def destroy
+      @artist = Artist.find(params[:id])
+      @artist.destroy
+      flash[:notice] = "Artist deleted."
       redirect_to artists_path
     end
-  end
 
-  def create
-    @artist = Artist.new(artist_params)
+    private
 
-    if @artist.save
-      redirect_to @artist
-    else
-      render :new
+    def artist_params
+      params.require(:artist).permit(:name)
     end
-  end
-
-  def edit
-    @artist = Artist.find(params[:id])
-  end
-
-  def update
-    @artist = Artist.find(params[:id])
-
-    @artist.update(artist_params)
-
-    if @artist.save
-      redirect_to @artist
-    else
-      render :edit
-    end
-  end
-
-  def destroy
-    @artist = Artist.find(params[:id])
-    @artist.destroy
-    flash[:notice] = "Artist deleted."
-    redirect_to artists_path
-  end
-
-  private
-
-  def artist_params
-    params.require(:artist).permit(:name)
-  end
 end
